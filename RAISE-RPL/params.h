dec365790a768c926ad1123c7451a1f6ee5556a8
pallavikaliyar <pallavi.kaliyar@gmail.com>
2018-03-13 17:01:39 +0100

    add files

---
+++ RAISE-RPL/params.h
@@ -0,0 +1,6 @@
+//Max number of good nodes.
+int good_upperLimit = 80;
+
+//Max number of bad nodes. Bad nodes are after good nodes.
+int bad_upperLimit = 20;
+
---
+++ rpl/params.h
@@ -0,0 +1,6 @@
+//Max number of good nodes.
+int good_upperLimittwo = 80;
+
+//Max number of bad nodes. Bad nodes are after good nodes.
+int bad_upperLimittwo = 20;
+

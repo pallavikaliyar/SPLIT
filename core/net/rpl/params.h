232c44fa8d4eb22e96ca161c06d568e7d77027f9
pallavikaliyar <pallavi.kaliyar@gmail.com>
2018-03-13 16:43:16 +0100

    delet

--- core/net/rpl/params.h
+++
@@ -1,6 +0,0 @@
-//Max number of good nodes.
-int good_upperLimittwo = 3;
-
-//Max number of bad nodes. Bad nodes are after good nodes.
-int bad_upperLimittwo = 3;
-
--- core/net/rpl/readme.txt
+++
@@ -1,4 +0,0 @@
-place files 
-	rpl-icmp6.c and params.h
-  of this zip file at path 
-	/home/user/contiki/core/net/rpl
